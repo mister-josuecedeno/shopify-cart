cf30333273e8cf89c32fc61e99abe78f82051b7b
josuecedeno <josuecedeno@yahoo.com>
2020-07-29 14:24:01 -0500

    Initial checkout

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
 import { Client as Styletron } from 'styletron-engine-atomic';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ShopProvider from '../context/shopContext';
+
 import HomePage from '../pages/HomePage';
 import ProductPage from '../pages/ProductPage';
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const debug =
   process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
@@ -13,18 +14,20 @@ const engine = new Styletron();
 
 function App() {
   return (
-    <StyletronProvider value={engine} debug={debug} debugAfterHydration>
-      <Router>
-        <Switch>
-          <Route path='/product'>
-            <ProductPage />
-          </Route>
-          <Route path='/'>
-            <HomePage />
-          </Route>
-        </Switch>
-      </Router>
-    </StyletronProvider>
+    <ShopProvider>
+      <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+        <Router>
+          <Switch>
+            <Route path='/product/:id'>
+              <ProductPage />
+            </Route>
+            <Route path='/'>
+              <HomePage />
+            </Route>
+          </Switch>
+        </Router>
+      </StyletronProvider>
+    </ShopProvider>
   );
 }
 
--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,10 +1,87 @@
 import React, { Component } from 'react';
 import Client from 'shopify-buy';
 
+const ShopContext = React.createContext();
+
+const client = Client.buildClient({
+  //domain: 'https://determined-hoover-3b9b83.netlify.app',
+  //storefrontAccessToken: '4389fc83c820e98f6e3a0b98917395e7',
+  storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
+  domain: 'graphql.myshopify.com',
+});
+
 class shopProvider extends Component {
+  state = {
+    products: [],
+    product: {},
+    checkout: {},
+    isCartOpen: false,
+    test: 'test',
+  };
+
+  componentDidMount() {
+    this.createCheckout();
+  }
+
+  createCheckout = async () => {
+    const checkout = await client.checkout.create();
+    this.setState({ checkout: checkout });
+  };
+
+  addItemToCheckout = async (variantId, quantity) => {
+    const lineItemsToAdd = [
+      {
+        variantId,
+        quantity: parseInt(quantity, 10),
+      },
+    ];
+
+    const checkout = await client.checkout.addLineItems(
+      this.state.checkout.id,
+      lineItemsToAdd
+    );
+
+    this.setState({ checkout: checkout });
+  };
+
+  fetchAllProducts = async () => {
+    const products = await client.product.fetchAll();
+    this.setState({ products: products });
+  };
+
+  fetchProductWithId = async (id) => {
+    const product = await client.product.fetch(id);
+    this.setState({ product: product });
+  };
+
+  closeCart = () => {
+    this.setState({ isCartOpen: false });
+  };
+
+  openCart = () => {
+    this.setState({ isCartOpen: true });
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <ShopContext.Provider
+        value={{
+          ...this.state,
+          fetchAllProducts: this.fetchAllProducts,
+          fetchProductWithId: this.fetchProductWithId,
+          closeCart: this.closeCart,
+          openCart: this.openCart,
+          addItemToCheckout: this.addItemToCheckout,
+        }}
+      >
+        {this.props.children}
+      </ShopContext.Provider>
+    );
   }
 }
+
+const ShopConsumer = ShopContext.Consumer;
+
+export { ShopConsumer, ShopContext };
 
 export default shopProvider;
