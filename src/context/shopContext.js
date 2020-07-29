9e471edaf8f4c0d35c99261841e7393a38ff42f2
josuecedeno <josuecedeno@yahoo.com>
2020-07-29 11:52:15 -0500

    First commit

---
+++ src/context/shopContext.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Client from 'shopify-buy';
+
+class shopProvider extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default shopProvider;
